1b6761803d3c69fbe03bc5fc08ca5ace7f6f7c99
Maxime Rossier <rossier.maxime@gmail.com>
2023-08-06 18:45:02 +0200

    show interest for assets

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -137,7 +137,7 @@ function updateItem () {
       <div v-if="copy.type === 'loan' && copy.duration">
         Paid in {{ copy.duration > 1 ? copy.duration.toFixed(0) + ' years' : Math.round(copy.duration * 12) + ' months' }}.
       </div>
-      <div v-if="copy.type === 'loan'">
+      <div v-if="copy.type !== 'expense'">
         <label for="interest">
           Interests
         </label>
